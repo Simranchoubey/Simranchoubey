e0578f31d905328032a67478528ea9399e674b90
Simranchoubey <simrankchoubey@gmail.com>
2024-07-27 17:18:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function moveRandomEl(elm) {
+
+  function moveRandomEl(elm) {
     elm.style.position = "absolute";
     elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
     elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
